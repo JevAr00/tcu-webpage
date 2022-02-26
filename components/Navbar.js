777ab905a98787038b882f7426fe908d99eabf21
Kevin Arias <kevinjosuearias@hotmail.com>
2022-02-25 18:16:21 -0600

    Primera prueba de Navbar funcionando

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,22 @@
+import styled from 'styled-components';
+import Link from 'next/link';
+
+const Nav = styled.nav`
+width: auto;
+height: 100px;
+background-color: #E1E5F2;
+`;
+
 const Navbar = () => {
 	return (
-		<div>
-		</div>
+		<header>
+			<Nav>
+				<Link href='/'>Inicio</Link>
+				<Link href='/nosotros'>Nosotros</Link>
+				<Link href='/docentes'>Docentes</Link>
+				<Link href='#'>Contacto</Link>
+			</Nav>
+		</header>
 	);
 };
 

bd62f1c224852105515cc79b7a7d09bbc068c36a
Kevin Arias <kevinjosuearias@hotmail.com>
2022-02-25 18:09:17 -0600

    Layout ahora contiene el Head

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Layout = ({ children }) => {
 	return (
 		<div>
+			<Head>
+				<title>Inicio</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 			<Navbar />
 			{ children }
 			<Footer />

8eacab039bd05180e54598bc58f53eb1d39a4f45
Kevin Arias <kevinjosuearias@hotmail.com>
2022-02-25 18:15:01 -0600

    Agregadas paginas de prueba

---
+++ pages/docentes.js
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+const Docentes = () => {
+	return (
+		<div>
+			<h1>Docentes</h1>
+		</div>
+	);
+};
+
+export default Docentes;
---
+++ pages/nosotros.js
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+const Nosotros = () => {
+	return (
+		<div>
+			<h1>Nosotros</h1>
+		</div>
+	);
+};
+
+export default Nosotros;

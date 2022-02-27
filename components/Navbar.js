a49d5e86be6eeb38a5615953759f7a93bf5012e7
Kevin Arias <kevinjosuearias@hotmail.com>
2022-02-27 17:50:25 -0600

    Creacion de navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,41 @@
 import styled from 'styled-components';
-import Link from 'next/link';
+import Link from 'next/Link';
+import { colors } from '../public/theme';
 
-const Nav = styled.nav`
-width: auto;
-height: 100px;
-background-color: #E1E5F2;
+const Header = styled.header`
+	display: flex;
+	width: 100%;
+	height: 64px;
+	padding: 0px 8%;
+	align-items: center;
+	justify-content: space-between;
+	background-color: ${colors.principal};
+`;
+
+const A = styled.a`
+	size: 0rem;
+	margin-left: 30px;
+	letter-spacing: 1px;
+	color: ${colors.secundario};
+
+	&:hover{
+		border-bottom: 2px solid ${colors.secundario};
+	}
 `;
 
 const Navbar = () => {
 	return (
-		<header>
-			<Nav>
-				<Link href='/'>Inicio</Link>
-				<Link href='/nosotros'>Nosotros</Link>
-				<Link href='/docentes'>Docentes</Link>
-				<Link href='#'>Contacto</Link>
-			</Nav>
-		</header>
+		<Header>
+			<div>
+				<p>Logo</p>
+			</div>
+			<nav>
+				<Link href='/'><A>Inicio</A></Link>
+				<Link href='/nosotros'><A>Nosotros</A></Link>
+				<Link href='/docentes'><A>Docentes</A></Link>
+				<Link href='#footer'><A>Contacto</A></Link>
+			</nav>
+		</Header>
 	);
 };
 

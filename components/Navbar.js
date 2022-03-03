43786b2438366ade104bb6b1720f0d91bed9704e
soju13 <jarotcb74@gmail.com>
2022-03-03 14:59:59 -0600

    Cambio de navbar a sticky

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,37 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Link from 'next/Link';
 import { colors, breakpoints } from '../public/theme';
 import { Menu } from '@styled-icons/evaicons-solid/Menu';
 
 const Header = styled.header`
+	width: 100%;
 	display: flex;
+	position: fixed;
+	transition: 0.6s;
 	align-items: center;
-	min-height: 5rem;
-	padding-inline: 4rem;
+	padding: 1.125rem 4.4rem;
 	justify-content: space-between;
-	background-color: ${colors.principal};
+	background-color: transparent;
 
+	.sticky{
+		background-color:white ;
+	}
 	nav{
 		ul{
 			margin: 0;
 			padding: 0;
 			display: flex;
-
+			align-items: center;
+			justify-content: center;
 			li{
+				margin: 0;
 				list-style: none;
+				position: relative;
 			}
 		}
 	}
 
-	@media (max-width: ${breakpoints.small}) {
+	@media (max-width: ${breakpoints.medium}) {
 		padding-inline: 1.5rem;
 
 		li{
@@ -32,8 +40,14 @@ const Header = styled.header`
 	}
 `;
 
+
 const Logo = styled.div`
+	color: white;
+	transition: 0.6s;
 	font-size: 1.5rem;
+	position: relative;
+	text-decoration: none;
+	letter-spacing: 0.125rem;
 `;
 
 const SMenu = styled(Menu)`
@@ -41,16 +55,20 @@ const SMenu = styled(Menu)`
 	height: 2rem;
 	display: none;
 
-	@media (max-width: ${breakpoints.small}) {
+	@media (max-width: ${breakpoints.medium}) {
 		display: flex;
 	}
 `;
 
 const A = styled.a`
-	letter-spacing: 0.1rem;
 	padding: 1rem;
-	color: ${colors.secundario};
 	cursor: pointer;
+	margin: 0 0.938rem;
+	position: relative;
+	text-decoration: none;
+	letter-spacing: 0.125rem;
+	color: ${colors.secundario};
+	
 
 	&:hover{
 		border-bottom: 1px solid ${colors.secundario};
@@ -65,7 +83,7 @@ const Navbar = () => {
 		<Header>
 			<Logo>Logo</Logo>
 			<nav>
-				<SMenu />
+				<SMenu/>
 				<ul>
 					<li><Link href='/'><A>Inicio</A></Link></li>
 					<li><Link href='/nosotros'><A>Nosotros</A></Link></li>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import Layout from '../components/Layout';
 import '../styles/globals.css';
+import Layout from '../components/Layout';
+import React, { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+
+	const [headerText, setHeaderText] = useState(false);
+
+	useEffect(() => {
+		const header = document.querySelector ('header');
+		window.addEventListener('scroll', () => {
+			if (window.scrollY > 0) {
+				header.classList.add('sticky');
+				if (headerText != true) {
+					setHeaderText(true);
+				}
+			}
+			else {
+				header.classList.remove('sticky');
+				if (headerText != false) {
+					setHeaderText(false);
+				}
+			}
+		});
+	});
+
 	return (
 		<Layout>
 			<Component {...pageProps} />

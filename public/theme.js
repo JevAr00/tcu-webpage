3ac5da40d4344b89c6202fa5de37fca062cbd206
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-02 20:43:40 -0600

    Agregado descripciones a los tamaños sobre su valor correspondiente

--- public/theme.js
+++ public/theme.js
@@ -7,8 +7,23 @@ export const colors = {
 };
 
 export const breakpoints = {
+	/**
+	 * @description 576px
+	 */
 	small: '576px',
+
+	/**
+	 * @description 768px
+	 */
 	medium: '768px',
+
+	/**
+	 * @description 992px
+	 */
 	large: '992px',
+
+	/**
+	 * @description 1200px
+	 */
 	x_large: '1200px',
 };

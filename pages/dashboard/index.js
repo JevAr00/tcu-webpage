270460e4a7651335bae44cf8b5ae5efc8f5cdc83
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-08 21:38:48 -0600

    SSR al dashboard. Ahora se verifica si el usuario esta autenticado

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,9 +1,14 @@
 import styles from 'styles/Dashboard.module.css';
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 export default function Dashboard() {
+
 	return (
 		<div className={styles.main}>
 			<h1>Hola</h1>
+			<a href='api/auth/logout'>Salir</a>
 		</div>
 	);
 }
+
+export const getServerSideProps = withPageAuthRequired();

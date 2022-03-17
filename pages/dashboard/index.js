08e66ae550a002bf5538012e60f8a85bffbf7498
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-17 14:44:04 -0600

    correcion en la ruta de logout

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,12 +1,12 @@
 import styles from 'styles/Dashboard.module.css';
-import { withPageAuthRequired, getSession } from '@auth0/nextjs-auth0';
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 export default function Dashboard() {
 
 	return (
 		<div className={styles.main}>
 			<h1>Hola</h1>
-			<a href='api/auth/logout'>Salir</a>
+			<a href='/logout'>Salir</a>
 		</div>
 	);
 }

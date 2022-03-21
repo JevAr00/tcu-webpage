406f926c66e401dd7914e99b176112923a8e8e22
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-21 00:04:22 -0600

    Prueba de componente Navbar

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,13 +1,16 @@
 import styles from 'styles/Dashboard.module.css';
+import { Navbar } from 'components/Dashboard';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 export default function Dashboard() {
 
 	return (
-		<div className={styles.main}>
-			<h1>Hola</h1>
-			<a href='/logout'>Salir</a>
-		</div>
+		<>
+			<Navbar />
+			<div className={styles.container}>
+				<h1>Hola</h1>
+			</div>
+		</>
 	);
 }
 

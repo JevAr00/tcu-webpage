11d412d2e22090b5a7bf790b875c2740f51b2bcc
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-21 12:57:47 -0600

    Se cambio la manera para exportar los iconos. Ahora pueden ser llamados por individual

--- components/icons/index.js
+++ components/icons/index.js
@@ -1,13 +1,11 @@
-import Phone from './Phone';
-import Email from './Email';
-import Location from './Location';
-import SFacebook from './Facebook';
-import SWaze from './Waze';
+import _Phone from './Phone';
+import _Email from './Email';
+import _Location from './Location';
+import _SFacebook from './Facebook';
+import _SWaze from './Waze';
 
-export const icons = {
-	Phone,
-	Email,
-	Location,
-	SFacebook,
-	SWaze,
-};
+export const Phone = _Phone;
+export const Email = _Email;
+export const Location = _Location;
+export const Facebook = _SFacebook;
+export const Waze = _SWaze;

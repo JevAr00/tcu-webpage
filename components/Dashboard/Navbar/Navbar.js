0e03db8c46a4d1d768b47d8723f959f7b637e66b
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-22 19:47:54 -0600

    Agregado boton a la UI

--- components/Dashboard/Navbar/Navbar.js
+++ components/Dashboard/Navbar/Navbar.js
@@ -1,9 +1,29 @@
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
+import { colors } from 'public/theme';
+import { Exit } from '../../Icons';
+
+const Header = styled.header`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding-inline: 4rem;
+	background-color: ${colors.principal};
+`;
+
 const Navbar = () => {
+	const Router = useRouter();
+	const handleClick = () => {
+		Router.push('/logout');
+	};
+
 	return (
-		<header>
+		<Header>
 			<h1>Dashboard</h1>
-			<a href='/logout'>Salir</a>
-		</header>
+			<Exit onClick={handleClick}/>
+		</Header>
 	);
 };
 

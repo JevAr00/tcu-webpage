78a6eb0d52a15703704aa86abbafc43b4bb836c2
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-23 13:31:47 -0600

    Corregido error en el import de Link. Navbar ahora sera tratado como panel lateral de control

--- components/Dashboard/Navbar.js
+++ components/Dashboard/Navbar.js
@@ -1,28 +1,51 @@
 import styled from 'styled-components';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import { colors } from 'public/theme';
 import { Exit } from '../Icons';
 
 const Header = styled.header`
-	width: 100%;
 	display: flex;
+	position: fixed;
+	flex-direction: column;
+	width: 14rem;
+	height: 100%;
 	align-items: center;
-	justify-content: space-between;
-	padding-inline: 4rem;
 	background-color: ${colors.principal};
 `;
 
-const Navbar = () => {
-	const Router = useRouter();
-	const handleClick = () => {
-		Router.push('/logout');
-	};
+const StyledExit = styled(Exit)`
+	position: absolute;
+	bottom: 2rem;
+`;
 
+const Option = styled.div`
+	margin: .5rem 0;
+	width: 90%;
+	height: 3rem;
+	text-align: center;
+	border-radius: 0.2rem;
+	backdrop-filter: blur(1rem);
+	background-color: hsl(0 0% 100% / 0.1);
+	border-bottom: 1px solid ${colors.secundario};
+`;
+
+const A = styled.a`
+	width: 100%;
+	height: 100%;
+`;
+
+const Navbar = () => {
 	return (
 		<Header>
 			<h1>Dashboard</h1>
-			<Exit onClick={handleClick}/>
+			<Option>
+				<a href=''><A>Docentes</A></a>
+			</Option>
+			<Option>
+				<a href=''>Prueba</a>
+			</Option>
+			<StyledExit href='/logout'/>
 		</Header>
 	);
 };

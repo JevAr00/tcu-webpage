0be6e2c05895da86e6ad8fa2d0662e89c618f083
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-25 12:32:59 -0600

    Ahora se muestra el panel de manera correcta al presionar una opcion

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,15 +1,80 @@
-import styles from 'styles/Dashboard.module.css';
-import { Navbar } from 'components/Dashboard';
+import styled from 'styled-components';
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { useState } from 'react';
+
+import { colors } from 'public/theme';
+import { Exit } from 'components/Icons';
+import { Panel } from 'components/Dashboard';
+import styles from 'styles/Dashboard.module.css';
+
+const Header = styled.header`
+	display: flex;
+	flex-direction: column;
+	width: 14rem;
+	height: 100%;
+	align-items: center;
+	justify-content: space-around;
+	background-color: ${colors.principal};
+
+	h1{
+		margin:0;
+		font-size: 1.5rem;
+	}
+`;
+
+const OptionContainer = styled.div`
+	display: flex;
+	align-items: flex-end;
+	flex-direction: column;
+	width: 100%;
+	height: 80%;
+`;
+
+const Option = styled.div`
+	width: 94%;
+	height: 3.3rem;
+	margin-bottom: 1rem;
+	text-align: center;
+	line-height: 3.2rem;
+	border-radius: 2rem 0 0 2rem;
+	background-color: white;
+`;
+
+const Button = styled.button`
+	font-size: 1.4rem;
+	width: 100%;
+	height: 100%;
+	color: ${colors.footer};
+	border: none;
+	background-color: transparent;
+	border-radius: 2rem 0 0 2rem;
+
+	&:hover{
+		cursor: pointer;
+	}
+`;
+
 
 export default function Dashboard() {
+	const [panel, setPanel] = useState(null);
+
+	const handleClick = (panelName) => {
+		setPanel(panelName);
+		console.log(panel);
+	};
 
 	return (
 		<div className={styles.container}>
-			<Navbar />
-			<div className={styles.tableContainer}>
-				a
-			</div>
+			<Header>
+				<h1>Dashboard</h1>
+				<OptionContainer>
+					<Option>
+						<Button onClick={() => handleClick('docente')}>Docentes</Button>
+					</Option>
+				</OptionContainer>
+				<Exit href='/logout'/>
+			</Header>
+			<Panel selectedPanel={panel}/>
 		</div>
 	);
 }

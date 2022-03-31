70bbd4705321ad7ccc7132aacc4f6174cec9da04
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-30 19:26:58 -0600

    Se agregaron datos de prueba que se pasan a la tabla

--- components/Dashboard/Panels/PDocentes.js
+++ components/Dashboard/Panels/PDocentes.js
@@ -1,7 +1,57 @@
 import styled from 'styled-components';
 import Table from '../Table';
 
-const headers = ['Nombre', 'Apellidos', 'Grado'];
+const headers = ['Nombre', 'Apellido', 'Materia', 'Grado'];
+const dbData = [
+	{ nombre:'Kevin', apellido:'Arias', materias: 'Computacion', grado: 'sexto' },
+	{ nombre:'Juan', apellido:'Perez', materias: 'Ingles', grado: 'primero' },
+	{ nombre:'Jacinto', apellido:'Jimenez', materias: 'Generales', grado: 'tercero' },
+	{ nombre:'Luis', apellido:'Delgado', materias: 'Computacion', grado: 'segundo' },
+	{ nombre:'David', apellido:'Hernandez', materias: 'Artes Plasticas', grado: 'quinto' },
+	{ nombre:'Jarot', apellido:'Carmiol', materias: 'Computacion', grado: 'cuarto' },
+	{ nombre:'Kevin', apellido:'Arias', materias: 'Computacion', grado: 'sexto' },
+	{ nombre:'Juan', apellido:'Perez', materias: 'Ingles', grado: 'primero' },
+	{ nombre:'Jacinto', apellido:'Jimenez', materias: 'Generales', grado: 'tercero' },
+	{ nombre:'Luis', apellido:'Delgado', materias: 'Computacion', grado: 'segundo' },
+	{ nombre:'David', apellido:'Hernandez', materias: 'Artes Plasticas', grado: 'quinto' },
+	{ nombre:'Jarot', apellido:'Carmiol', materias: 'Computacion', grado: 'cuarto' },
+	{ nombre:'Kevin', apellido:'Arias', materias: 'Computacion', grado: 'sexto' },
+	{ nombre:'Juan', apellido:'Perez', materias: 'Ingles', grado: 'primero' },
+	{ nombre:'Jacinto', apellido:'Jimenez', materias: 'Generales', grado: 'tercero' },
+	{ nombre:'Luis', apellido:'Delgado', materias: 'Computacion', grado: 'segundo' },
+	{ nombre:'David', apellido:'Hernandez', materias: 'Artes Plasticas', grado: 'quinto' },
+	{ nombre:'Jarot', apellido:'Carmiol', materias: 'Computacion', grado: 'cuarto' },
+	{ nombre:'Kevin', apellido:'Arias', materias: 'Computacion', grado: 'sexto' },
+	{ nombre:'Juan', apellido:'Perez', materias: 'Ingles', grado: 'primero' },
+	{ nombre:'Jacinto', apellido:'Jimenez', materias: 'Generales', grado: 'tercero' },
+	{ nombre:'Luis', apellido:'Delgado', materias: 'Computacion', grado: 'segundo' },
+	{ nombre:'David', apellido:'Hernandez', materias: 'Artes Plasticas', grado: 'quinto' },
+	{ nombre:'Jarot', apellido:'Carmiol', materias: 'Computacion', grado: 'cuarto' },
+	{ nombre:'Kevin', apellido:'Arias', materias: 'Computacion', grado: 'sexto' },
+	{ nombre:'Juan', apellido:'Perez', materias: 'Ingles', grado: 'primero' },
+	{ nombre:'Jacinto', apellido:'Jimenez', materias: 'Generales', grado: 'tercero' },
+	{ nombre:'Luis', apellido:'Delgado', materias: 'Computacion', grado: 'segundo' },
+	{ nombre:'David', apellido:'Hernandez', materias: 'Artes Plasticas', grado: 'quinto' },
+	{ nombre:'Jarot', apellido:'Carmiol', materias: 'Computacion', grado: 'cuarto' },
+	{ nombre:'Kevin', apellido:'Arias', materias: 'Computacion', grado: 'sexto' },
+	{ nombre:'Juan', apellido:'Perez', materias: 'Ingles', grado: 'primero' },
+	{ nombre:'Jacinto', apellido:'Jimenez', materias: 'Generales', grado: 'tercero' },
+	{ nombre:'Luis', apellido:'Delgado', materias: 'Computacion', grado: 'segundo' },
+	{ nombre:'David', apellido:'Hernandez', materias: 'Artes Plasticas', grado: 'quinto' },
+	{ nombre:'Jarot', apellido:'Carmiol', materias: 'Computacion', grado: 'cuarto' },
+	{ nombre:'Kevin', apellido:'Arias', materias: 'Computacion', grado: 'sexto' },
+	{ nombre:'Juan', apellido:'Perez', materias: 'Ingles', grado: 'primero' },
+	{ nombre:'Jacinto', apellido:'Jimenez', materias: 'Generales', grado: 'tercero' },
+	{ nombre:'Luis', apellido:'Delgado', materias: 'Computacion', grado: 'segundo' },
+	{ nombre:'David', apellido:'Hernandez', materias: 'Artes Plasticas', grado: 'quinto' },
+	{ nombre:'Jarot', apellido:'Carmiol', materias: 'Computacion', grado: 'cuarto' },
+	{ nombre:'Kevin', apellido:'Arias', materias: 'Computacion', grado: 'sexto' },
+	{ nombre:'Juan', apellido:'Perez', materias: 'Ingles', grado: 'primero' },
+	{ nombre:'Jacinto', apellido:'Jimenez', materias: 'Generales', grado: 'tercero' },
+	{ nombre:'Luis', apellido:'Delgado', materias: 'Computacion', grado: 'segundo' },
+	{ nombre:'David', apellido:'Hernandez', materias: 'Artes Plasticas', grado: 'quinto' },
+	{ nombre:'Jarot', apellido:'Carmiol', materias: 'Computacion', grado: 'cuarto' },
+];
 
 const Area = styled.div`
 	height: 100%;
@@ -12,7 +62,7 @@ const Area = styled.div`
 const PDocentes = () => {
 	return (
 		<Area>
-			<Table columns={headers}/>
+			<Table data={dbData} columns={headers}/>
 		</Area>
 	);
 };

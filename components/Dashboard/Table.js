93c13f3b234724eced46bb0e516fc20364d05496
Kevin Arias <kevinjosuearias@hotmail.com>
2022-03-30 19:30:04 -0600

    Creacion de tabla. Prueba de diseño y la persentacion de informacion

--- components/Dashboard/Table.js
+++ components/Dashboard/Table.js
@@ -1,7 +1,42 @@
-const Table = ({ data, columns }) => {
+import styled from 'styled-components';
+import { colors } from 'public/theme';
+
+const Table = styled.table`
+	width: 100%;
+	padding: 4rem 2rem;
+	table-layout: fixed;
+`;
+
+const THead = styled.thead`
+	background-color: ${colors.secundario};
+
+	th{
+			padding: 20px 15px;
+  		text-align: center;
+  		font-weight: 500;
+  		font-size: 12px;
+  		color: #fff;
+  		text-transform: uppercase;
+		}
+`;
+
+const TBody = styled.tbody`
+	height: 100px;
+  overflow-x: auto;
+	background-color: ${colors.principal};
+
+	td {
+  		padding: 15px;
+  		text-align: left;
+  		vertical-align: middle;
+  		font-weight: 300;
+		}
+`;
+
+const dataTable = ({ data, columns }) => {
 	return (
-		<table>
-			<thead>
+		<Table>
+			<THead>
 				<tr>
 					{
 						columns.map((columna) => (
@@ -9,28 +44,21 @@ const Table = ({ data, columns }) => {
 						))
 					}
 				</tr>
-			</thead>
+			</THead>
 			<tbody>
-				<tr>
-					<td>1</td>
-					<td>Mark</td>
-					<td>Otto</td>
-					<td>@mdo</td>
-				</tr>
-				<tr>
-					<td>2</td>
-					<td>Jacob</td>
-					<td>Thornton</td>
-					<td>@fat</td>
-				</tr>
-				<tr>
-					<td>3</td>
-					<td colSpan={2}>Larry the Bird</td>
-					<td>@twitter</td>
-				</tr>
+				{
+					data.map((datos) => (
+						<tr key={datos}>
+							<td>{datos.nombre}</td>
+							<td>{datos.apellido}</td>
+							<td>{datos.materias}</td>
+							<td>{datos.grado}</td>
+						</tr>
+					))
+				}
 			</tbody>
-		</table>
+		</Table>
 	);
 };
 
-module.exports = Table;
+module.exports = dataTable;

2925e1e29d7f606ea7396918ee9015ee30271b09
soju13 <jarotcb74@gmail.com>
2022-07-03 13:16:13 -0600

    Cambio: Misión y Visión

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { motion, Variant } from 'framer-motion';
 
 export default function Home() {
 
-	const textAnimate = {
+	const fadeIn = {
 		offScreen:{ y:100, opacity: 0 },
 		onScreen:{ y:0, opacity: 1,
 			transition:{ type: 'spring',
@@ -33,18 +33,18 @@ export default function Home() {
 			<motion.div className={styles.mision_motion}
 				initial={'offScreen'}
 				whileInView={'onScreen'}
-				viewport={{ once:false, amount: 0.4 }}
+				viewport={{ once:false, amount: 0.5 }}
 				transition={{ staggerChildren: 0.5 }}
 			>
 
 				<motion.div className={styles.tit_mivi}
-					variants={textAnimate}
+					variants={fadeIn}
 				>
 					<h2>Misión</h2>
 				</motion.div>
 
 				<motion.div className={styles.tex_mivi}
-					variants={textAnimate}
+					variants={fadeIn}
 				>
 					<p >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
 				</motion.div>
@@ -53,18 +53,18 @@ export default function Home() {
 			<motion.div className={styles.vision_motion}
 				initial={'offScreen'}
 				whileInView={'onScreen'}
-				viewport={{ once:false, amount: 0.3 }}
+				viewport={{ once:false, amount: 0.5 }}
 				transition={{ staggerChildren: 0.5 }}
 			>
 
 				<motion.div className={styles.tit_mivi}
-					variants={textAnimate}
+					variants={fadeIn}
 				>
 					<h2>Visión</h2>
 				</motion.div>
 
 				<motion.div className={styles.tex_mivi}
-					variants={textAnimate}
+					variants={fadeIn}
 				>
 					<p >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
 				</motion.div>

e648543c029e8df9caf96a1913bbb0480bc62eab
alejimenez1 <alejandrodelgadold116@gmail.com>
2022-08-06 00:32:13 -0600

    conexion a BD

--- utils/db/dbConnect.js
+++ utils/db/dbConnect.js
@@ -2,22 +2,45 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-const conectarDB = async () => {
-	try {
-		await mongoose.connect(MONGODB_URI, {
+
+if (!MONGODB_URI) {
+	throw new Error(
+		'Please define the MONGODB_URI environment variable inside .env.local',
+	);
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+	cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+	if (cached.conn) {
+		return cached.conn;
+	}
+
+	if (!cached.promise) {
+		const opts = {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			bufferCommands: false,
-			bufferMaxEntries: 0,
-			useFindAndModify: false,
-			useCreateIndex: true,
-		});
-		console.log('mongodb conectado ');
-	}
-	catch (error) {
-		console.log(error);
-		process.exit(1);
-	}
-};
 
-export default conectarDB;
+
+		};
+
+		cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongo) => {
+			return mongo;
+		});
+	}
+	cached.conn = await cached.promise;
+	return cached.conn;
+}
+
+export default dbConnect;
+

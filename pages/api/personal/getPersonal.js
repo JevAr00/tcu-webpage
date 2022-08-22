6d4247401ee842d4ae3f99347bccbbded9190078
alejimenez1 <alejandrodelgadold116@gmail.com>
2022-08-22 00:41:17 -0600

    se limpio el get de codigo innecesario

--- pages/api/personal/getPersonal.js
+++ pages/api/personal/getPersonal.js
@@ -1,25 +1,24 @@
 import Personal from 'utils/db/models/personal';
 import conectarDB from 'utils/db/dbConnect';
-import { withApiAuthRequired } from '@auth0/nextjs-auth0';
+
 
 export default async function handler1(req, res) {
-	const { method } = req;
+
 
 	// DB Connect
 	await conectarDB();
 
 	// Get method
 
-	if (method === 'GET') {
-		try {
-			const personal = await Personal.find();
 
-			res.status(200).json(personal);
-		}
-		catch (err) {
-			res.status(500).json(err);
-		}
+	try {
+		const personal = await Personal.find();
 
+		res.status(200).json(personal);
 	}
+	catch (err) {
+		res.status(500).json(err);
+	}
+
 
 }

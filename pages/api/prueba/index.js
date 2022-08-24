8e350ceee18b0eecf3b0b5a8e2529962eed38aab
Kevin Arias <kevinjosuearias@hotmail.com>
2022-08-23 21:38:54 -0600

    eliminado archivo de prueba de ruta protegida

--- pages/api/prueba/index.js
+++
@@ -1,6 +0,0 @@
-import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
-
-export default withApiAuthRequired(async function rutaPrueba(req, res) {
-	const { user } = getSession(req, res);
-	res.json({ protected: 'My Secret', nombre: user.nickname });
-});
